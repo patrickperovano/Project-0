ff573da0e3dfd18e2c30ce39acdcf34a17374bcf
Patrick Perovano <53125389+patrickperovano@users.noreply.github.com>
2024-08-10 14:12:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+#>>>>>>>>>>>>>>>>>>>>>>>>> INTRODUCAO A PYTHON <<<<<<<<<<<<<<<<<<<<<<<<<#
+
+#>>> 1. Commentarios <<<<
+
+#>>> 2. Representacao de Strings<<<
+
+#>>> 3. Hello World<<<
+#print ('Hello World')<<<
+
+#>>> 4. Possiveis erros
+#>>> 4.1. Faltando parantese
+#>>> 4.2. P em vez de p
+#>>> 4.3. Faltando aspa
+#>>> 4.4. Erro ortogrfico?
+
+#>>> 5. Saida c/ varias linhas
+#print ('Primeira Linha\nSegunda Linha')
+#print ('''Primeira Linha
+#Segunda Linha''')
+
+#>>> 6. Execucao passo-a-passo
+#print ('Primeira Linha')
+#print ('Segunda Linha')
+
+#>>> 7. Historico de Versoes
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>> PRIMEIROS PROGRAMAS EM PYTHON <<<<<<<<<<<<<<<<<<<<#
+
+#PARTE 1
+
+# 1. Como definir identificadores de variavies
+#num_pessoas
+
+# 2. Como criar variaveis
+#num_pessoas = 20000
+#personagem = 'Mario'
+#vidas = 6
+#moedas = 20
+#pontos = 10000
+#tempo = 246.57
+
+# 3. Como uysar o valor de uma variavel
+#print (num_pessoas)
+#print (personagem)
+#print (vidas)
+#print (moedas)
+#print (pontos)
+#print (tempo)
+
+# 4. Exemplo: "Hello World" personalizado
+#nome = 'Patrick Perovano'
+#print ('Ola,', nome)
+#print (f'Ola, {nome}')
+
+#PARTE 2
+
+# 1. Como alterar o valor de uma variavel
+
+# 2. Analisar variaveis usando debugger
